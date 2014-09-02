54cb3de13adf0bb6b8af020b53015889e9fc6a65
Dragan Savic <savic021@gmail.com>
2014-09-02 17:12:23 +0200

    added: saving data as JSON to internal phone memory

--- app/src/main/java/com/example/dragan/gps_mapa/Parcela.java
+++ app/src/main/java/com/example/dragan/gps_mapa/Parcela.java
@@ -1,41 +1,78 @@
 package com.example.dragan.gps_mapa;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class Parcela extends Activity {
 
-    ArrayList<LatLng> mlista;
+    ArrayList<LatLng> parcela_lista;
+    ArrayAdapter<String> adapter;
     private ListView lv;
     ArrayAdapter mAdapter;
-    Spinner spiner;
+    Spinner spinner;
+    EditText poljeOznaka, povrsinaZemljista;
+    CheckBox box;
+    String s, po, spn, pz, b;
+    boolean cb ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        mlista =  (ArrayList<LatLng>)intent.getSerializableExtra("listaMarkera");
+        parcela_lista =  (ArrayList<LatLng>)intent.getSerializableExtra("listaMarkera");
         setContentView(R.layout.activity_parcela);
 
        /* lv = (ListView)findViewById(R.id.fieldInfo_listview);
-
         mAdapter = new ArrayAdapter (this,android.R.layout.simple_list_item_1, mlista);
         lv.setAdapter(mAdapter); */
 
-        Spinner spinner = (Spinner)findViewById(R.id.tip_zemljista);
+        poljeOznaka = (EditText)findViewById(R.id.polje_oznaka);
+        spinner = (Spinner)findViewById(R.id.tip_zemljista);
+        povrsinaZemljista = (EditText)findViewById(R.id.povrsinaZemljista);
+        box = (CheckBox)findViewById(R.id.checkbox_omogucena_parcela);
+        box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(box.isChecked()) {
+                    cb = true;
+                }
+                else {
+                    cb = false;
+                }
+            }
+        });
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tip_zemljista, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+
+
     }
 
 
@@ -56,5 +93,51 @@ public class Parcela extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void writeJSON() {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put("oznaka_parcele", po);
+            jsonObject.put("tip_zemljista_parcele", spn);
+            jsonObject.put("povrsina_parcele", pz);
+            jsonObject.put("omogucena--parcela", cb);
+            Log.d("ujson", jsonObject.toString());
+            JSONArray jsonArray = new JSONArray();
+            for(int i = 0; i < parcela_lista.size(); i++) {
+                JSONArray tmp = new JSONArray();
+                tmp.put( parcela_lista.get(i).latitude);
+                tmp.put( parcela_lista.get(i).longitude);
+                jsonArray.put(tmp);
+            }
+            jsonObject.put("koordinate_parcele", jsonArray);
+            Log.d("json", jsonObject.toString());
+            String data = jsonObject.toString();
+            try {
+                FileOutputStream fOut = openFileOutput("podaci_parcela",MODE_PRIVATE);
+                fOut.write(data.getBytes());
+                fOut.close();
+                Toast.makeText(getBaseContext(),"file saved",
+                        Toast.LENGTH_SHORT).show();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void saveParcela(View view) {
+        po = poljeOznaka.getText().toString();
+        spn = spinner.getSelectedItem().toString();
+        pz = povrsinaZemljista.getText().toString();
+        Log.d("testis", "provera");
+        writeJSON();
+
+        Toast.makeText(getApplicationContext(),"cuvanje",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/dragan/gps_mapa/Parcelica.java
+++ app/src/main/java/com/example/dragan/gps_mapa/Parcelica.java
@@ -1,17 +1,54 @@
 package com.example.dragan.gps_mapa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 
 
 public class Parcelica extends Activity {
 
+    ArrayList<LatLng> parcelica_lista;
+    EditText oznakaParcelica, povrsinaParcelice;
+    CheckBox boxParcelica;
+    boolean cbp ;
+    String op, pp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        parcelica_lista =  (ArrayList<LatLng>)intent.getSerializableExtra("listaMarkera");
         setContentView(R.layout.activity_parcelica);
+
+        oznakaParcelica = (EditText)findViewById(R.id.oznaka_parcelica);
+        povrsinaParcelice = (EditText)findViewById(R.id.povrsina_parcelica);
+        boxParcelica = (CheckBox)findViewById(R.id.checkbox_omogucena_parcelica);
+        boxParcelica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(boxParcelica.isChecked()) {
+                    cbp = true;
+                } else {
+                    cbp = false;
+                }
+            }
+        });
+
     }
 
 
@@ -32,5 +69,48 @@ public class Parcelica extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void saveParcelica(View view) {
+        op = oznakaParcelica.getText().toString();
+        pp = povrsinaParcelice.getText().toString();
+
+        writeJSONP();
+        Toast.makeText(getApplicationContext(),"cuvanje",Toast.LENGTH_SHORT).show();
+    }
+
+    public void writeJSONP() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("oznaka_parcelice", op);
+            jsonObject.put("povrsina_parcelice", pp);
+            jsonObject.put("omogucena--parcelica", cbp);
+            Log.d("ujson", jsonObject.toString());
+            JSONArray jsonArray = new JSONArray();
+            for(int i = 0; i < parcelica_lista.size(); i++) {
+                JSONArray tmp = new JSONArray();
+                tmp.put( parcelica_lista.get(i).latitude);
+                tmp.put( parcelica_lista.get(i).longitude);
+                jsonArray.put(tmp);
+            }
+            jsonObject.put("koordinate_parcelice", jsonArray);
+            Log.d("json", jsonObject.toString());
+
+            String data = jsonObject.toString();
+            try {
+                FileOutputStream fOut = openFileOutput("podaci_parcelica",MODE_PRIVATE);
+                fOut.write(data.getBytes());
+                fOut.close();
+                Toast.makeText(getBaseContext(),"file saved",
+                        Toast.LENGTH_SHORT).show();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
